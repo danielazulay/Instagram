d4c70877ff0a270a22b359f3a4c0f4d1a9fbb0ab
danielazulay <danielazulay20@gmail.com>
2024-06-13 19:35:34 +0300

    Updates

--- src/cmps/buttons/CircleImg.jsx
+++
@@ -1,6 +0,0 @@
-export function CircleImg({img,height,width}) {
-   return(
-      <img className="logo" src={img} alt="imgProfile" style={{ height: `${height}px`, width: `${width}px` }} />
-
-   )
-}
